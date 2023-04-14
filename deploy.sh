2981a51265822d3a3858dd1e2b19f58d2fd05cb5
mikilukasik <szarhazi@gmail.com>
2023-04-14 12:59:19 +0100

    use public ecr to save £0.06 per month

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
-aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 600160064813.dkr.ecr.eu-west-1.amazonaws.com
+# ECR private
+#
+# aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 600160064813.dkr.ecr.eu-west-1.amazonaws.com
+# npm run build
+# docker build --platform=linux/amd64 -t docker-image:test .
+# docker tag docker-image:test 600160064813.dkr.ecr.eu-west-1.amazonaws.com/test:latest
+# docker push 600160064813.dkr.ecr.eu-west-1.amazonaws.com/test:latest                 
 
+# ECR public
+#
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/y7b1h4a4
 npm run build
-
-docker build --platform=linux/amd64 -t docker-image:test .
-# docker run -p 9000:8080 docker-image:test         
-
-docker tag docker-image:test 600160064813.dkr.ecr.eu-west-1.amazonaws.com/test:latest
-
-docker push 600160064813.dkr.ecr.eu-west-1.amazonaws.com/test:latest                 
-
+docker build --platform=linux/amd64 -t chss-lambda-ai .
+docker tag chss-lambda-ai:latest public.ecr.aws/y7b1h4a4/chss-lambda-ai:latest
+docker push public.ecr.aws/y7b1h4a4/chss-lambda-ai:latest
